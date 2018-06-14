2e2c7cf6d6bc5c71f885d6eb5504af81bf0b2117
mikedelong <mikedelong@outlook.com>
2018-06-14 11:46:13 -0400

    feature: load into a data frame and produce some summary information.

--- code/read_and_model.py
+++ code/read_and_model.py
@@ -1,5 +1,8 @@
+# https://blog.socialcops.com/engineering/machine-learning-python/
 import logging
 import time
+
+import pandas as pd
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -16,10 +19,16 @@ if __name__ == '__main__':
     input_file = '../input/titanic3.xls'
     logger.debug('reading data from %s' % input_file)
 
+    titanic_df = pd.read_excel(input_file, encoding='ISO-8859-1')
+    logger.debug('data has shape %d x %d' % titanic_df.shape)
+    logger.debug('data has columns %s' % titanic_df.columns.values)
+    logger.debug('mean by passenger class: \n%s' % titanic_df.groupby('pclass').mean())
+    logger.debug('survived mean: %.4f' % titanic_df['survived'].mean())
+
     logger.debug('done')
     finish_time = time.time()
     elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
     elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
-    logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+    logger.info('Time: {:0>2}:{:0>2}:{:05.2f}'.format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
     console_handler.close()
     logger.removeHandler(console_handler)
