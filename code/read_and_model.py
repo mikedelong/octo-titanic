6a80912b6cb683cfac14f5f3a82d5420da9ac231
mikedelong <mikedelong@outlook.com>
2018-06-14 13:03:12 -0400

    feature: loop through several columns and predict them instead of just predicting the one target

--- code/read_and_model.py
+++ code/read_and_model.py
@@ -42,15 +42,16 @@ if __name__ == '__main__':
     t0 = titanic_df.drop(partial_columns, axis=1)
     t0['home.dest'] = t0['home.dest'].fillna('NA')
     t0 = t0.dropna()
-
     processed_df = preprocess_titanic_df(t0)
-    X = processed_df.drop(['survived'], axis=1).values
-    y = processed_df['survived'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    clf_dt = DecisionTreeClassifier(max_depth=10)
 
-    clf_dt.fit(X_train, y_train)
-    logger.debug('score: %.4f' % clf_dt.score(X_test, y_test))
+    for target_column in ['embarked', 'pclass', 'survived', 'sex']:
+        X = processed_df.drop([target_column], axis=1).values
+        y = processed_df[target_column].values
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        clf_dt = DecisionTreeClassifier(max_depth=10)
+
+        clf_dt.fit(X_train, y_train)
+        logger.debug('target: %s score: %.4f' % (target_column, clf_dt.score(X_test, y_test)))
 
     logger.debug('done')
     finish_time = time.time()
