7c8cf08bb5c7b2206e20b4e51b3cbc70684c18b8
mikedelong <mikedelong@outlook.com>
2018-06-14 12:51:41 -0400

    feature: first full working version that uses a decision tree classifer to predict on 20% of the data.

--- code/read_and_model.py
+++ code/read_and_model.py
@@ -3,6 +3,19 @@ import logging
 import time
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+
+
+def preprocess_titanic_df(df):
+    result = df.copy()
+    label_encoder = LabelEncoder()
+    result.sex = label_encoder.fit_transform(result.sex)
+    result.embarked = label_encoder.fit_transform(result.embarked)
+    result = result.drop(['name', 'ticket', 'home.dest'], axis=1)
+    return result
+
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -24,6 +37,20 @@ if __name__ == '__main__':
     logger.debug('data has columns %s' % titanic_df.columns.values)
     logger.debug('mean by passenger class: \n%s' % titanic_df.groupby('pclass').mean())
     logger.debug('survived mean: %.4f' % titanic_df['survived'].mean())
+
+    partial_columns = ['body', 'cabin', 'boat']
+    t0 = titanic_df.drop(partial_columns, axis=1)
+    t0['home.dest'] = t0['home.dest'].fillna('NA')
+    t0 = t0.dropna()
+
+    processed_df = preprocess_titanic_df(t0)
+    X = processed_df.drop(['survived'], axis=1).values
+    y = processed_df['survived'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    clf_dt = DecisionTreeClassifier(max_depth=10)
+
+    clf_dt.fit(X_train, y_train)
+    logger.debug('score: %.4f' % clf_dt.score(X_test, y_test))
 
     logger.debug('done')
     finish_time = time.time()
